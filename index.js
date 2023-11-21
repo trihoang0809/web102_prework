a5d08442c2e3e8a51716a1f8e7c3fb4f87ea9de7
Tri Hoang <trihoang0809@gmail.com>
2023-11-21 00:33:54 -0600

    finish c7

--- index.js
+++ index.js
@@ -187,7 +187,14 @@ const sortedGames =  GAMES_JSON.sort( (item1, item2) => {
 });
 
 // use destructuring and the spread operator to grab the first and second games
+const [firstGame, secondGame, ...otherGames] = sortedGames;
+// console.log(firstGame.name);
+// console.log(secondGame.name);
 
 // create a new element to hold the name of the top pledge game, then append it to the correct element
+const firstGameName = firstGame.name;
+firstGameContainer.append(firstGameName);
 
 // do the same for the runner up item
+const secondGameName = secondGame.name;
+secondGameContainer.append(secondGameName);
