6d505ec1ab7c8570a31e3c8ef41467121b13a655
Tri Hoang <79836132+trihoang0809@users.noreply.github.com>
2023-11-20 22:19:50 -0600

    Update index.js

--- index.js
+++ index.js
@@ -67,19 +67,33 @@ addGamesToPage(GAMES_JSON);
 const contributionsCard = document.getElementById("num-contributions");
 
 // use reduce() to count the number of total contributions by summing the backers
+const totalContributions = GAMES_JSON.reduce((total, game) => {
+    return total + game.backers;
+  }, 0);  
 
 
 // set the inner HTML using a template literal and toLocaleString to get a number with commas
-
+contributionsCard.innerHTML = totalContributions.toLocaleString('en-US')
 
 // grab the amount raised card, then use reduce() to find the total amount raised
 const raisedCard = document.getElementById("total-raised");
 
-// set inner HTML using template literal
+const totalRaised = GAMES_JSON.reduce((total, game) => {
+    return total + game.pledged;
+  }, 0);  
 
+// set inner HTML using template literal
+raisedCard.innerHTML = `$${totalRaised.toLocaleString('en-US')}`;
 
 // grab number of games card and set its inner HTML
 const gamesCard = document.getElementById("num-games");
+
+const numGames = GAMES_JSON.reduce((total) => {
+    return total + 1;
+  }, 0);  
+
+// set inner HTML using template literal
+gamesCard.innerHTML = numGames.toLocaleString('en-US');
 
 
 /*************************************************************************************
