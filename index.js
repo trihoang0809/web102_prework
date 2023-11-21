8176451ce5e27079f7d1fb3958a15327c1262982
Tri Hoang <79836132+trihoang0809@users.noreply.github.com>
2023-11-20 19:34:17 -0600

    Update index.js

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ function addGamesToPage(games) {
 }
 // call the function we just defined using the correct variable
 // later, we'll call this function using a different list of games
-
+addGamesToPage(GAMES_JSON);
 
 /*************************************************************************************
  * Challenge 4: Create the summary statistics at the top of the page displaying the
